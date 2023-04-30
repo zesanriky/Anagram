763ffec795a772032211b15a139e01bbc780d370
zesanriky <zesanriky@gmail.com>
2023-05-01 00:07:43 +0200

    initial commit

---
+++ src/main/java/com/assignment/anagrams/anagram_solver/AnagramCheckingApp.java
@@ -0,0 +1,97 @@
+package com.assignment.anagrams.anagram_solver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnagramCheckingApp {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		while (true) {
+
+			// Expecting the first String input from the user
+			System.out.println("Please enter the first String or type 'exit' to to close this program");
+			String firstString = checkInputValidation(input.nextLine());
+
+			// This program will be terminated if the user enters 'exit'
+			if (firstString.equalsIgnoreCase("exit")) {
+
+				System.out.println("Good bye see you again, your program has been terminated.");
+				break;
+			}
+
+			// Expecting the second String input from the user
+			System.out.println("Please enter the second String");
+			String secondString = checkInputValidation(input.nextLine());
+
+			// Now comparing between the two Strings if they are anagrams or not.
+
+			if (isAnagram(firstString, secondString)) {
+
+				System.out.println("Your given two Strings are anagrams");
+
+			}
+
+			else {
+
+				System.out.println("The given Strings are not anagrams");
+
+			}
+		}
+		// Closing the user input
+
+		input.close();
+
+	}
+
+	// Checking the given Strings anagrams possibility
+
+	public static boolean isAnagram(String firstString, String secondString) {
+
+		// Conversion from strings to character arrays
+
+		char[] firstArray = firstString.toCharArray();
+		char[] secondArray = secondString.toCharArray();
+
+		// Sorting out the arrays with the help of a method
+
+		Arrays.sort(firstArray);
+		Arrays.sort(secondArray);
+
+		return Arrays.equals(firstArray, secondArray);
+	}
+
+	// Checking is the user has only entered the alphabetic characters.
+	public static String checkInputValidation(String input) {
+
+		// Removing any leading/trailing white space and converting to lower case
+		String modifyInput = input.trim().toLowerCase();
+
+		// Defining regular expression for accepting alphabetic characters only.
+		String regex = "^[a-z]+$";
+
+		// Help of regular expression to match inputs
+
+		if (modifyInput.matches(regex)) {
+
+			return modifyInput;
+
+		} else {
+			System.out.println("Sorry wrong input, please enter only alphabetic characters.");
+
+			Scanner inputRepeat = new Scanner(System.in);
+
+			String userInput = inputRepeat.nextLine();
+
+			return checkInputValidation(userInput);
+
+		}
+
+	}
+
+}
---
+++ src/test/java/com/assignment/anagrams/anagram_solver/AnagramCheckingAppTest.java
@@ -0,0 +1,117 @@
+package com.assignment.anagrams.anagram_solver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AnagramCheckingAppTest {
+
+	@Test
+	void testAnagramCheckingApp() {
+
+		// Case1: Checking two anagrams
+
+		String firstString = "silent";
+		String secondString = "listen";
+		boolean expectedResultForTrue = true;
+		boolean actualResultForTrue = AnagramCheckingApp.isAnagram(firstString, secondString);
+		Assertions.assertEquals(expectedResultForTrue, actualResultForTrue);
+
+		// Case2: Checking two anagrams
+
+		String thirdString = "cat";
+		String fourthString = "dog";
+		boolean expectedResultForFalse = false;
+		boolean actualResultForFalse = AnagramCheckingApp.isAnagram(thirdString, fourthString);
+		Assertions.assertEquals(expectedResultForFalse, actualResultForFalse);
+
+	}
+
+	@Test
+	void testIsAnagram() {
+
+		// Testing for anagram validity
+
+		assertTrue(AnagramCheckingApp.isAnagram("listen", "silent"));
+
+		assertTrue(AnagramCheckingApp.isAnagram("debit card", "bad credit"));
+
+		assertTrue(AnagramCheckingApp.isAnagram("the eyes", "they see"));
+
+		// Testing for invalidity
+
+		assertFalse(AnagramCheckingApp.isAnagram("hello", "world"));
+
+		assertFalse(AnagramCheckingApp.isAnagram("abcd", "abcc"));
+
+		assertFalse(AnagramCheckingApp.isAnagram("tree", "three"));
+
+	}
+
+	@Test
+	void testCheckInputValidationPositive() {
+
+		// Testing input validity
+
+		String userInput = "anagram";
+		String expected = "anagram";
+		String actual = AnagramCheckingApp.checkInputValidation(userInput);
+		assertEquals(expected, actual);
+
+	}
+
+	// Test for valid input containing only alphabetic characters
+
+	// To test these cases the user needs to give input
+	// 1st input: abcdefghi, 2nd input = abcd and 3rd input = abcd
+	// Then the test should pass 100%
+	// It fails if the user input inputs are not expected.
+
+	@Test
+	public void testValidInput() {
+		String input = "abcdefg";
+		String expectedOutput = "abcdefg";
+		String actualOutput = AnagramCheckingApp.checkInputValidation(input);
+		assertEquals(expectedOutput, actualOutput);
+	}
+
+	// Test for invalid input containing non-alphabetic characters
+	@Test
+	public void testInvalidInput() {
+		String input = "1234567";
+		String expectedOutput = "abcd";
+		String actualOutput = AnagramCheckingApp.checkInputValidation(input);
+		assertEquals(expectedOutput, actualOutput);
+	}
+
+	// Test for input containing spaces
+	@Test
+	public void testInputWithSpaces() {
+		String input = " abc def ghi ";
+		String expectedOutput = "abcdefghi";
+		String actualOutput = AnagramCheckingApp.checkInputValidation(input);
+		assertEquals(expectedOutput, actualOutput);
+	}
+
+	// Test for input containing mixed case characters
+	@Test
+	public void testInputWithMixedCase() {
+		String input = "AbCdEfG";
+		String expectedOutput = "abcdefg";
+		String actualOutput = AnagramCheckingApp.checkInputValidation(input);
+		assertEquals(expectedOutput, actualOutput);
+	}
+
+	// Test for input containing special characters
+	@Test
+	public void testInputWithSpecialCharacters() {
+		String input = "@#$%^&*()_+-=`~[]\\{}|;':\",./<>?";
+		String expectedOutput = "abcd";
+		String actualOutput = AnagramCheckingApp.checkInputValidation(input);
+		assertEquals(expectedOutput, actualOutput);
+	}
+
+}
